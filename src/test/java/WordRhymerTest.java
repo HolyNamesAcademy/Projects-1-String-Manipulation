3574c5b90961bc7b0bca0063e50968bf99b57a41
Halston Crites <hcrites@amazon.com>
2018-12-16 16:52:33 -0800

    Update run configurations

--- src/test/java/WordRhymerTest.java
+++ src/test/java/WordRhymerTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Test class for WhordRhymer
  */
@@ -9,42 +12,42 @@ public class WordRhymerTest {
 
     @Test
     public void checkForRhyme_rhymes_bothWordsThreeOrMorePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_notRhymes_bothWordsThreeOrMorePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_rhymes_wordLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_notRhymes_wordLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_rhymes_possibleRhymeLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_notRhymes_possibleRhymeLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_rhymes_bothLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
     @Test
     public void checkForRhyme_notRhymes_bothLessThanThreePhonemes() {
-
+        assertTrue(false);
     }
 
 }
