fd8bca92ab7b326617adc20f162643aa49862aec
Jeff Tyson <jtyson@holynames-sea.org>
2020-01-07 22:43:01 -0800

    Fix project up for 2020 (#7)
    Fixes include
    1. Add more tests
    2. Refactor PhonemeDictionary to only use contructor (no wannabe factory)
    3. Add PhonemeDictionaryUtilities
    4. Add PhonemeDictionaryUtilitiesTest
    5. Write AAA for all tests
    6. Write psuedo code for all un-written tests
    7. Clean-up minor formatting here and there

--- src/test/java/RhymeGeneratorTest.java
+++ src/test/java/RhymeGeneratorTest.java
@@ -13,61 +13,76 @@ public class RhymeGeneratorTest {
 
     @Test
     public void findRhymingWords_oneRequestedAtLeastOneExists() {
+        // Arrange
         String wordToRhymeWith = "Boast";
         int numberOfRhymesRequested = 1;
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, numberOfRhymesRequested);
 
+        // Assert
         assertTrue(String.format("Expected %d rhyme when requesting %d rhyme for %s, but received %d",
-            numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith, rhymes.size()),
-            rhymes.size() == numberOfRhymesRequested);
+                numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith, rhymes.size()),
+                rhymes.size() == numberOfRhymesRequested);
     }
 
     @Test
     public void findRhymingWords_oneRequestedNoneExist() {
+        // Arrange
         String wordToRhymeWith = "Balogna";
         int numberOfRhymesRequested = 1;
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, numberOfRhymesRequested);
 
+        // Assert
         assertTrue(String.format("Expected no rhymes when requesting %d rhyme for %s, but received %d",
-            numberOfRhymesRequested, wordToRhymeWith, rhymes.size()), rhymes.size() == 0);
+                numberOfRhymesRequested, wordToRhymeWith, rhymes.size()), rhymes.size() == 0);
     }
 
     @Test
     public void findRhymingWords_multipleRequestedMultipleExist() {
+        // Arrange
         String wordToRhymeWith = "Plaid";
         int numberOfRhymesRequested = 3;
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, numberOfRhymesRequested);
 
+        // Assert
         assertTrue(String.format("Expected %d rhymes when requesting %d rhymes for %s, but received %d",
-            numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith, rhymes.size()), rhymes.size() == 3);
+                numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith, rhymes.size()), rhymes.size() == 3);
     }
 
     @Test
     public void findRhymingWords_multipleRequestedMoreThanExist() {
+        // Arrange
         String wordToRhymeWith = "Dipole";
         int numberOfRhymesRequested = 15;
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, numberOfRhymesRequested);
 
+        // Assert
         assertTrue(String.format("Expected less than %d rhymes when requesting %d "
-            + "rhymes for %s, received: %d", numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith,
-            rhymes.size()), rhymes.size() < numberOfRhymesRequested);
+                        + "rhymes for %s, received: %d", numberOfRhymesRequested, numberOfRhymesRequested, wordToRhymeWith,
+                rhymes.size()), rhymes.size() < numberOfRhymesRequested);
         assertTrue(String.format("Expected more than zero rhymes when requesting %d "
-            + "rhymes for  %s, received: %d", numberOfRhymesRequested, wordToRhymeWith, rhymes.size()),
-            rhymes.size() > 0);
+                        + "rhymes for  %s, received: %d", numberOfRhymesRequested, wordToRhymeWith, rhymes.size()),
+                rhymes.size() > 0);
 
     }
 
     @Test
     public void sanityTest1_findRhymingWords_validateCertainRhymesComeBack() {
+        // Arrange
         String wordToRhymeWith = "complicate";
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, 500);
         System.out.println(rhymes.size());
 
+        // Assert
         assertFalse("A word should not rhyme with itself.", listContains(rhymes, wordToRhymeWith));
         assertTrue("Expected indicate to rhyme with complicate.", listContains(rhymes, "indicate"));
         assertTrue("Expected locate to rhyme with complicate.", listContains(rhymes, "locate"));
@@ -78,11 +93,14 @@ public class RhymeGeneratorTest {
 
     @Test
     public void sanityTest2_findRhymingWords_validateCertainRhymesComeBack() {
+        // Arrange
         String wordToRhymeWith = "dance";
 
+        // Act
         List<String> rhymes = rhymeGenerator.findRhymingWords(wordToRhymeWith, 500);
         System.out.println(rhymes.size());
 
+        // Assert
         assertFalse("A word should not rhyme with itself.", listContains(rhymes, wordToRhymeWith));
         assertTrue("Expected lance to rhyme with dance.", listContains(rhymes, "lance"));
         assertTrue("Expected france to rhyme with dance.", listContains(rhymes, "france"));
@@ -91,12 +109,11 @@ public class RhymeGeneratorTest {
     }
 
     private boolean listContains(List<String> wordList, String wordToFind) {
-        for(String word: wordList){
-            if(word.equalsIgnoreCase(wordToFind)){
+        for (String word : wordList) {
+            if (word.equalsIgnoreCase(wordToFind)) {
                 return true;
             }
         }
         return false;
     }
-
 }
--- src/test/java/WordRhymerTest.java
+++ src/test/java/WordRhymerTest.java
@@ -4,51 +4,128 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Test class for WhordRhymer
+ * Test class for WordRhymer
  */
 public class WordRhymerTest {
 
     private WordRhymer wordRhymer = new WordRhymer();
 
     @Test
-    public void checkForRhyme_wordDoesNotRhymeWithSelf()  {
-        //TODO - implement me!
+    //TODO - implement me!
+    public void checkForRhyme_wordDoesNotRhymeWithSelf() {
+        // Arrange
+        //// Choose a word
+        //// Choose a possible rhyme that is the same as above word
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_rhymes_bothWordsThreeOrMorePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with three or more phonemes
+        //// Choose a rhyming word with three or more phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_notRhymes_bothWordsThreeOrMorePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with three or more phonemes
+        //// Choose a non-rhyming word with three or more phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_rhymes_wordLessThanThreePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with less than three phonemes
+        //// Choose a rhyming word with three or more phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_notRhymes_wordLessThanThreePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with less than three phonemes
+        //// Choose a non-rhyming word with three or more phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_rhymes_possibleRhymeLessThanThreePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with with three or more phonemes
+        //// Choose a rhyming word with less than three phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 
     @Test
+    //TODO - implement me!
     public void checkForRhyme_notRhymes_possibleRhymeLessThanThreePhonemes() {
-        //TODO - implement me!
+        // Arrange
+        //// Choose a word with with three or more phonemes
+        //// Choose a non-rhyming word with less than three phonemes
+
+        // Act
+        //// Call checkForRhyme with the above inputs
+
+        // Assert
+        //// Check checkForRhyme returned the correct result
+
+        //// Delete the line below once you start writing the real test
         assertTrue(false);
     }
 }
