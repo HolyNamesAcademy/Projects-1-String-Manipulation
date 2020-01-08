f9d955f8002b2f1f4fc04d6dd17bd810fc7067e2
Jeff Tyson <tysonyo@gmail.com>
2020-01-07 22:35:45 -0800

    Fix project up for 2020
    Fixes include
    1. Add more tests
    2. Refactor PhonemeDictionary to only use contructor (no wannabe factory)
    3. Add PhonemeDictionaryUtilities
    4. Add PhonemeDictionaryUtilitiesTest
    5. Write AAA for all tests
    6. Write psuedo code for all un-written tests
    7. Clean-up minor formatting here and there

--- src/main/java/PhonemeDictionary.java
+++ src/main/java/PhonemeDictionary.java
@@ -9,71 +9,15 @@ import java.util.Scanner;
 /** The representation of the CMU pronunciation dictionary. */
 public class PhonemeDictionary {
 
-    // You can edit the file path here to use "resources/miniCmuDict.txt" if you want to test
-    // against the smaller dictionary
-    private static final String DICTIONARY_FILE_PATH = "resources/cmudict.0.7a.txt";
-
-    private static PhonemeDictionary instance = null;
+    private static final String DEFAULT_DICTIONARY_FILE_PATH = "resources/cmudict.0.7a.txt";
     private Map<String, List<String>> phonemeDictionary;
 
-    public static PhonemeDictionary getDictionary() {
-        if(instance == null) {
-            return new PhonemeDictionary(DICTIONARY_FILE_PATH);
-        }
-
-        return instance;
+    public PhonemeDictionary() {
+        this(DEFAULT_DICTIONARY_FILE_PATH);
     }
 
-    /**
-     * Getter all entries in the phoneme dictionary. This is the set of words that the dictionary has phonemes for.
-     * @return the list of all words in our dictionary
-     */
-    public List<String> getEntries() {
-        return new ArrayList<>(phonemeDictionary.keySet());
-    }
-
-    /**
-     * Returns the list of phonemes that make up the word being passed in.
-     * @param word - the word to get phonemes for.
-     * @return a list of phonemes associated with word. An empty list is returned if the word is unrecognized.
-     */
-    public List<String> getPhonemes(String word){
-        return phonemeDictionary.getOrDefault(word.toUpperCase(), new ArrayList<>());
-    }
-
-    /**
-     * Checks that the provided line is an entry in the file that contains a word and phonemes, and that it is not a
-     * line containing comments.
-     * @param line - a line from the dictionary being parsed
-     * @return true if the given line contains a word entry, false otherwise
-     */
-    private boolean isPhonemeEntry(String line) {
-        //TODO - implement me!
-        return false;
-    }
-
-    /**
-     * Get the word component of a dictionary line. Lines are of the format: WORD PH1 PH2 PH3
-     * @param line - the dictionary line we want to extract the word from
-     * @return the word part of line
-     */
-    private String getWordFromLine(String line) {
-        //TODO - implement me!
-        return "";
-    }
-
-    /**
-     * Get the phonemes component from a dictionary line. Lines are of the format: WORD PH1 PH2 PH3
-     * @param line - the dictionary line we want to get the phonemes from
-     * @return a list containing the phonemes of the word in order.
-     */
-    private List<String> getPhonemesFromLine(String line) {
-        //TODO - implement me!
-        List<String> phonemeList = new ArrayList<>();
-        return phonemeList;
-    }
-
-    protected PhonemeDictionary(String dictionaryFilePath) {
+    // Use this constructor directly if you want to test with a different (e.g. smaller) dictionary
+    public PhonemeDictionary(String dictionaryFilePath) {
         phonemeDictionary = new HashMap<>();
 
         File f = new File(dictionaryFilePath);
@@ -87,8 +31,8 @@ public class PhonemeDictionary {
             // Loop through the lines in the file and add the actual word entries to dictLines.
             while (scanner.hasNext()) {
                 String line = scanner.nextLine().trim();
-                if (isPhonemeEntry(line)) {
-                    phonemeDictionary.put(getWordFromLine(line), getPhonemesFromLine(line));
+                if (PhonemeDictionaryUtilities.isPhonemeEntry(line)) {
+                    phonemeDictionary.put(PhonemeDictionaryUtilities.getWordFromLine(line), PhonemeDictionaryUtilities.getPhonemesFromLine(line));
                 }
             }
         } catch (FileNotFoundException e) {
@@ -101,5 +45,24 @@ public class PhonemeDictionary {
             System.out.println("Unable to produce rhymes at this time!");
             System.exit(1);
         }
+    }
+
+    /**
+     * Getter all entries in the phoneme dictionary. This is the set of words that the dictionary has phonemes for.
+     *
+     * @return the list of all words in our dictionary
+     */
+    public List<String> getEntries() {
+        return new ArrayList<>(phonemeDictionary.keySet());
+    }
+
+    /**
+     * Returns the list of phonemes that make up the word being passed in.
+     *
+     * @param word - the word to get phonemes for.
+     * @return a list of phonemes associated with word. An empty list is returned if the word is unrecognized.
+     */
+    public List<String> getPhonemes(String word) {
+        return phonemeDictionary.getOrDefault(word.toUpperCase(), new ArrayList<>());
     }
 }
--- src/main/java/WordRhymer.java
+++ src/main/java/WordRhymer.java
@@ -6,10 +6,11 @@ import java.util.List;
  */
 public class WordRhymer {
 
-    private PhonemeDictionary phonemeDictionary = PhonemeDictionary.getDictionary();
+    private PhonemeDictionary phonemeDictionary = new PhonemeDictionary();
 
     /**
      * Returns the list of all words that are available for rhyming.
+     *
      * @return the list of all possible rhymes
      */
     public List<String> getAllPossibleRhymes() {
@@ -20,12 +21,13 @@ public class WordRhymer {
      * Determines whether two words rhyme. A rhyme is determined by matching the final phonemes of the words.
      * Two words will be considered rhymes if their final three phonemes match. If either word has fewer than three
      * phonemes, all phonemes for the word with fewer than three phonemes must match at the end of the other word.
-     * @param word - the phonemes for the word to be rhymed with
+     *
+     * @param word          - the phonemes for the word to be rhymed with
      * @param possibleRhyme - the phonemes for the word being considered a rhyme
      * @return true if possibleRhyme is a rhyme with word as determined by the phoneme matching rule noted above
      */
+    //TODO - implement me!
     public boolean checkForRhyme(String word, String possibleRhyme) {
-        //TODO - implement me!
         return false;
     }
 }
